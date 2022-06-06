654a3d4dd6fd74059e4176a42fc6f27940bc3cef
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-06-06 15:39:58 -0700

    Header and Footer working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Header from './Header/Header.js';
+import Footer from './Footer/Footer.js';
+import Main from './Main/Main.js';
+import { zodiac } from './data.js';
 {/* import the zodiac array */}
 
 function App() {
   return (
     <div className="App">
+      <Header 
+        name= 'Colter'/>
+      <Main 
+        zodiacs={zodiac} />
+      <Footer 
+        year='2022'/>
       {/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
